6a4350ee30d082affdf86501797eccfaf3c9a2af
Rick <ps.maniac@hotmail.com>
2020-12-17 10:46:53 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>UhMami Building...</h2>
+        <a
+          className="App-link"
+          href="https://www.facebook.com/UhMami-Cocina-105748524690274"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          UhMami Facebook
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
